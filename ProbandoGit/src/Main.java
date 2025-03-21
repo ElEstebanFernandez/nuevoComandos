687f0ff93b8a8d61910370f6d6bec5bf0e4be7c0
Esteban <metalyfierros@hotmail.es>
2025-03-21 20:05:34 +0100

    mi primer commit

---
+++ ProbandoGit/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i =0; i < 100; i++) {
+            switch (i){
+                case 1:{
+                    System.out.println("1");
+                    break;
+                }
+                case 10:{
+                    System.out.println("10");
+                    break;
+                }
+                case 20:{
+                    System.out.println("30");
+                    break;
+                }
+            }
+        }
+    }
+}
